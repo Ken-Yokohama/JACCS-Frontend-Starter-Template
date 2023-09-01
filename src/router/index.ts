d49e74f4f3e2cea69c574912a1cf0c9525df56de
Ken <kenlopezyokohama@gmail.com>
2023-09-01 11:22:46 +0800

    Add PostCSS Nesting and Media Min-Max package and code samples

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,8 +4,14 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: () => import('../modules/Home/Home.vue') },
-    { path: '/review-page-1', component: () => import('../modules/ReviewPage1/ReviewPage1.vue') },
-    { path: '/review-page-2', component: () => import('../modules/ReviewPage2/ReviewPage2.vue') },
+    {
+      path: '/review-page-1',
+      component: () => import('../modules/ReviewPage1/ReviewPage1.vue'),
+    },
+    {
+      path: '/review-page-2',
+      component: () => import('../modules/ReviewPage2/ReviewPage2.vue'),
+    },
     {
       path: '/sample-pinia-1',
       component: () => import('../modules/SamplePinia1/SamplePinia1.vue'),
@@ -13,6 +19,10 @@ const router = createRouter({
     {
       path: '/sample-pinia-2',
       component: () => import('../modules/SamplePinia2/SamplePinia2.vue'),
+    },
+    {
+      path: '/sample-postcss',
+      component: () => import('../modules/SamplePostCSS/SamplePostCSS.vue'),
     },
     {
       path: '/:catchAll(.*)',
